1de8adee136fd67d050e64af81df19d83f1e4ff0
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-27 15:26:19 -0400

    Update time-keeper.js

--- wdsub/time-keeper.js
+++ wdsub/time-keeper.js
@@ -20,8 +20,8 @@ if (!hasInitDate) {
 }
 
 let timeSpent = document.getElementById("time-spent");
-let numHours = Math.floor(timeDifference / 360000);
-let numMins = Math.floor((timeDifference % 360000) * 60);
+let numHours = Math.floor(timeDifference / 3600000);
+let numMins = Math.floor((timeDifference % 3600000) / 60000);
 let diffStr = numHours + ":" + numMins;
 timeSpent.innerText = diffStr;
 
